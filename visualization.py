ac7bdee3df27750d1b1a8734d85fd96f961f33b2
Michael Wang <mmwang@berkeley.edu>
2016-11-12 15:18:21 -0800

    ummmmm

--- visualization.py
+++ visualization.py
@@ -1,6 +1,8 @@
 import json
 from watson_developer_cloud import ToneAnalyzerV3
-
+import numpy as np
+import matplotlib as pl
+#import pylab as pl
 
 tone_analyzer = ToneAnalyzerV3(
     username="4411cbd2-1327-4c3a-95ff-afccc0893be3",
@@ -10,17 +12,63 @@ tone_analyzer = ToneAnalyzerV3(
 tonedict = tone_analyzer.tone(text='I am worried about the US.')["document_tone"]["tone_categories"][0]["tones"]
 for tone in tonedict:
 	if tone["tone_name"] == "Anger":
-		anger = tone["score"]
+		anger = tone["score"] * 100
 		print("anger: " + str(anger))
 	if tone["tone_name"] == "Disgust":
-		disgust = tone["score"]
+		disgust = tone["score"] * 100
 		print("disgust: " + str(disgust))
 	if tone["tone_name"] == "Fear":
-		fear = tone["score"] 
+		fear = tone["score"] * 100
 		print("fear: " + str(fear))
 	if tone["tone_name"] == "Joy":
-		joy = tone["score"] 
+		joy = tone["score"] * 100
 		print("joy: " + str(joy))
 	if tone["tone_name"] == "Sadness":
-		sadness = tone["score"] 
+		sadness = tone["score"] * 100
 		print("sadness: " + str(sadness))
+
+# Create Radar Chart Object
+class Radar(object):
+
+    def __init__(self, fig, titles, labels, rect=None):
+        if rect is None:
+            rect = [0.05, 0.05, 0.95, 0.95]
+
+        self.n = len(titles)
+        self.angles = np.arange(90, 90+360, 360.0/self.n)
+        self.axes = [fig.add_axes(rect, projection="polar", label="axes%d" % i) 
+                         for i in range(self.n)]
+
+        self.ax = self.axes[0]
+        self.ax.set_thetagrids(self.angles, labels=titles, fontsize=14)
+
+        for ax in self.axes[1:]:
+            ax.patch.set_visible(False)
+            ax.grid("off")
+            ax.xaxis.set_visible(False)
+
+        for ax, angle, label in zip(self.axes, self.angles, labels):
+            ax.set_rgrids(range(1, 6), angle=angle, labels=label)
+            ax.spines["polar"].set_visible(False)
+            ax.set_ylim(0, 5)
+
+    def plot(self, values, *args, **kw):
+        angle = np.deg2rad(np.r_[self.angles, self.angles[0]])
+        values = np.r_[values, values[0]]
+        self.ax.plot(angle, values, *args, **kw)
+
+fig = pl.figure(figsize=(6, 6))
+
+titles = ["Anger", "Disgust", "Fear", "Joy", "Sadness"]
+
+labels = [
+    ["20", "40", "60", "80", "100"], list("12345"), list("uvwxy"), 
+    ["one", "two", "three", "four", "five"],
+    list("jklmn")
+]
+
+radar = Radar(fig, titles, labels)
+radar.plot([1, 3, 2, 5, 4],  "-", lw=2, color="b", alpha=0.4, label="mood")
+radar.ax.legend()
+
+fig.show()

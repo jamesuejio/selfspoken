8dca85a193812dd1f054a59fc3dec6386aa54a01
Cindy Tung <cindytung@berkeley.edu>
2016-11-12 17:36:07 -0800

    renders radar graph

--- flaskr/flaskr/static/RadarChartScript.js
+++ flaskr/flaskr/static/RadarChartScript.js
@@ -3,9 +3,10 @@ var w = 500,
 
 var colorscale = d3.scale.category10();
 
-//Legend titles
-// var LegendOptions = ['Smartphone','Tablet'];
-
+function receiveData(data) {
+	console.log(data)
+	return data
+}
 //Data
 var d = [
 	[
@@ -51,31 +52,31 @@ var svg = d3.select('#body')
 // 	.text("What % of owners use a specific service in a week");
 
 //Initiate Legend
-var legend = svg.append("g")
-	.attr("class", "legend")
-	.attr("height", 100)
-	.attr("width", 200)
-	.attr('transform', 'translate(90,20)')
-	;
+// var legend = svg.append("g")
+// 	.attr("class", "legend")
+// 	.attr("height", 100)
+// 	.attr("width", 200)
+// 	.attr('transform', 'translate(90,20)')
+// 	;
 	//Create colour squares
-	legend.selectAll('rect')
-	  .data(LegendOptions)
-	  .enter()
-	  .append("rect")
-	  .attr("x", w - 65)
-	  .attr("y", function(d, i){ return i * 20;})
-	  .attr("width", 10)
-	  .attr("height", 10)
-	  .style("fill", function(d, i){ return colorscale(i);})
-	  ;
+	// legend.selectAll('rect')
+	//   .data(LegendOptions)
+	//   .enter()
+	//   .append("rect")
+	//   .attr("x", w - 65)
+	//   .attr("y", function(d, i){ return i * 20;})
+	//   .attr("width", 10)
+	//   .attr("height", 10)
+	//   .style("fill", function(d, i){ return colorscale(i);})
+	//   ;
 	//Create text next to squares
-	legend.selectAll('text')
-	  .data(LegendOptions)
-	  .enter()
-	  .append("text")
-	  .attr("x", w - 52)
-	  .attr("y", function(d, i){ return i * 20 + 9;})
-	  .attr("font-size", "11px")
-	  .attr("fill", "#737373")
-	  .text(function(d) { return d; })
-	  ;
+	// legend.selectAll('text')
+	//   .data(LegendOptions)
+	//   .enter()
+	//   .append("text")
+	//   .attr("x", w - 52)
+	//   .attr("y", function(d, i){ return i * 20 + 9;})
+	//   .attr("font-size", "11px")
+	//   .attr("fill", "#737373")
+	//   .text(function(d) { return d; })
+	//   ;

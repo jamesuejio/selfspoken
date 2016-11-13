1c16f4304423c2df409485a487b3e117a7577c32
James Uejio <jamesuejio@berkeley.edu>
2016-11-13 03:47:17 -0800

    Fix graph

---
+++ flaskr/flaskr/static/radarAggregations.js
@@ -0,0 +1,42 @@
+var w = 500,
+	h = 500;
+
+var colorscale = d3.scale.category10();
+var currentText = ""
+
+$.getJSON($SCRIPT_ROOT + '/getEmotionVals', function (data) {
+	createGraph(data);
+});
+
+function createGraph(emotionVals) {
+	axes = [];
+	for (var key in emotionVals){
+		var attrValue = emotionVals[key];
+	    axes.push({axis:key, value:attrValue});
+	}
+	// console.log(axes);
+	var d = [axes];
+
+	//Options for the Radar chart, other than default
+	var mycfg = {
+	  w: w,
+	  h: h,
+	  maxValue: 0.6,
+	  levels: 6,
+	  ExtraWidthX: 200
+	}
+
+	//Call function to draw the Radar chart
+	//Will expect that data is in %'s
+	RadarChart.draw("#chart", d, mycfg);
+
+	////////////////////////////////////////////
+	/////////// Initiate legend ////////////////
+	////////////////////////////////////////////
+
+	var svg = d3.select('#body')
+		.selectAll('svg')
+		.append('svg')
+		.attr("width", w)
+		.attr("height", h)
+}
